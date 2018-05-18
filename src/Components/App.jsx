1f391dc7e9592c3e72f7cb12f75cba852afcf64a
Aleksandar Ristic <storiaca@gmail.com>
2018-05-18 09:34:02 +0200

    Animal list reducer

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import AnimalList from './AniamalList';
+
 class App extends Component {
 
     render() {
         return (
           <div className="container app-container">
             <h4>App component</h4>
+						<AnimalList />
           </div>
         )
     }
---
+++ src/reducers/animalReducer.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+			{species: 'ape', name: 'hans', gender: 'm', age: 23},
+			{species: 'tortoise', name: 'heide', gender: 'f', age: 89},
+			{species: 'cat', name: 'nini', gender: 'f', age: 13},
+			{species: 'shark', name: 'gert', gender: 'm', age: 4}
+    ]
+}
