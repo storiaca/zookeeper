9f3ac65e63658f8eb93a9c16d275c5780368e167
Aleksandar Ristic <storiaca@gmail.com>
2018-05-18 10:37:50 +0200

    Using connect to bind props

--- src/Components/AniamalList.jsx
+++ src/Components/AniamalList.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class AnimalList extends Component {
+
   render() {
+		console.log('animal list', this.props);
+
     return(
       <div>AnimalList Component</div>
     );
   }
 }
 
-export default AnimalList;
+function mapStateToProps(state) {
+	return {
+		foobar: 'kitt',
+	}
+}
+
+//export default AnimalList;
+export default connect(mapStateToProps)(AnimalList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
+import animalReducer from './animalReducer';
+
+
 const rootReducer = combineReducers({
-    state: (state = {}) => state
+  //state: (state = {}) => state
+  animals: animalReducer
 })
 
 export default rootReducer; 
