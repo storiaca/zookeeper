02ab40dc43bb701a8d14897b74faa7c6f59a0a17
Aleksandar Ristic <storiaca@gmail.com>
2018-05-21 12:47:39 +0200

    Add faker npm

---
+++ src/api_server/seeds.js
@@ -0,0 +1,26 @@
+module.exports = function () {
+	let faker = require('faker');
+	let _ = require('lodash');
+
+	return {
+		animals: _.times(100, (num) => {
+			return {
+				id: num,
+				name: faker.name.findName(),
+				species: faker.company.catchPhraseNoun(),
+				gender: faker.helpers.randomize(['m', 'f']),
+				age: faker.random.number(100)
+			}
+		}),
+		zookeeper: _.times(20, (i) => {
+			return {
+				id: i,
+				name: {
+					firstName: faker.name.firstName(),
+					lastName: faker.name.lastName()
+				},
+				salary: faker.random.number(5000, 20000)
+			}
+		})
+	}
+}
